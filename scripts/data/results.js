6dcc8a532d267e19b7c4ad13e007faeac7431764
jarrisonn <j.h.m.t.v.10@gmail.com>
2021-04-24 13:55:27 -0300

    Update results.js

--- scripts/data/results.js
+++ scripts/data/results.js
@@ -1,3 +1,16 @@
 results = [
-    
+    {
+        team1: {
+            id: "juventus",
+            goals: 1,
+            who: [
+                "Cristiano Ronaldo"
+            ]
+        },
+        team2: {
+            id: "inter_milano",
+            goals: 0,
+            who: []
+        }
+    }
 ]
